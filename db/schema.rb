fe4e35d3f3bebd21742c2384b9dd7ccd3d2f716c
Vlad Kostikov <vladkostikovru@gmail.com>
2022-12-28 05:56:52 +0300

    Создал полиморфный Comment и связал с Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_28_024219) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_28_025419) do
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.string "commentable_type"
+    t.integer "commentable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
